25310c74ad2ed7e6af7e5ab9d9026db963c7f4c5
AxelNAY <axelnay@sfr.fr>
2024-09-26 01:12:12 +0200

    Fixing the exercice 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,12 +12,16 @@ const Statistics = (props) => {
   else {
     return (
       <div>
-        <StatisticLine text="good" value ={props.good} />
-        <StatisticLine text="neutral" value ={props.neutral} />
-        <StatisticLine text="bad" value ={props.bad} />
-        <StatisticLine text="all" value ={props.good + props.neutral + props.bad} />
-        <StatisticLine text="average" value ={((1 / props.good) + (1 / props.neutral) + (1 / props.bad)) / 3} />
-        <StatisticLine text="positive" value ={(props.good * 100) / (props.good + props.neutral + props.bad)} />
+        <table>
+          <tbody>
+            <StatisticLine text="good" value ={props.good} />
+            <StatisticLine text="neutral" value ={props.neutral} />
+            <StatisticLine text="bad" value ={props.bad} />
+            <StatisticLine text="all" value ={props.good + props.neutral + props.bad} />
+            <StatisticLine text="average" value ={((1 / props.good) + (1 / props.neutral) + (1 / props.bad)) / 3} />
+            <StatisticLine text="positive" value ={(props.good * 100) / (props.good + props.neutral + props.bad)} />
+          </tbody>
+        </table>
       </div>
     )
   }
